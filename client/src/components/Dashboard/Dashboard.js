ad1462fee6522039ee208c1cba77d73d6be3f528
David Johnson-Bau <dgbau@me.com>
2020-03-15 11:06:25 -0600

    added baisc server file and React skeleton files

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Dashboard.scss";
+
+function Dashboard(props) {
+    console.log(props)
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div className="widget">
+            <h3>Map</h3>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="widget">
+            <h3>Widget 1</h3>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="widget">
+            <h3>Widget 2</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-4">
+          <div className="widget">
+            <h3>Cam 1</h3>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="widget">
+            <h3>Cam 2</h3>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="widget">
+            <h3>Cam 3</h3>
+          </div>
+        </div>
+      </div>
+      <div>
+          <p>{props.data}</p>
+          <p>{props.data2.string1}</p>
+          <p>{props.data2.string2}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ client/src/components/MainNav/MainNav.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "./MainNav.scss";
+
+const NavItem = props => {
+  const pageURI = window.location.pathname + window.location.search;
+  const liClassName = props.path === pageURI ? "nav-item active" : "nav-item";
+  const aClassName = props.disabled ? "nav-link disabled" : "nav-link";
+  return (
+    <li className={liClassName}>
+      <a href={props.path} className={aClassName}>
+        {props.name}
+        {props.path === pageURI ? (
+          <span className="sr-only">(current)</span>
+        ) : (
+          ""
+        )}
+      </a>
+    </li>
+  );
+};
+
+class NavDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: false
+    };
+  }
+  showDropdown(e) {
+    e.preventDefault();
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+  render() {
+    const classDropdownMenu = "dropdown-menu" + (this.state.isToggleOn ? " show" : "");
+    console.log('call nav')
+    return (
+      <li className="nav-item dropdown">
+        <a
+          className="nav-link dropdown-toggle"
+          href="/"
+          id="navbarDropdown"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          onClick={e => {
+            this.showDropdown(e);
+          }}
+        >
+          {this.props.name}
+        </a>
+        <div className={classDropdownMenu} aria-labelledby="navbarDropdown">
+          {this.props.children}
+        </div>
+      </li>
+    );
+  }
+}
+
+function MainNav(props) {
+  return (
+    <nav className="navbar navbar-expand">
+      <a className="navbar-brand" href="/">
+        <img src="/images/trex.png" alt="DP logo" width="60px" height="auto"/>
+      </a>
+
+      <div className="navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          
+          {/* <NavItem path="/" name="Home" /> */}
+          {/* <NavItem path="/about" name="About" /> */}
+          <NavItem path="/" name="Home" />
+          {/* <NavDropdown name="Artists">
+            <a className="dropdown-item" href="/Artist1">
+              Artist 1
+            </a>
+            <a className="dropdown-item" href="/Artist2">
+              Artist 2
+            </a>
+            <a className="dropdown-item" href="/Artist3">
+              Artist 3
+            </a>
+          </NavDropdown> */}
+          <NavItem path="/clients" name="Clients" />
+          <NavItem path="/map" name="Map" />
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default MainNav;
