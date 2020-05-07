31e4b9d877c4e26711e523223f9b3562fd09874f
David Johnson-Bau <dgbau@me.com>
2020-05-07 15:16:07 -0600

    changed fakeData.js

--- client/src/fakeData.js
+++ client/src/fakeData.js
@@ -9,97 +9,103 @@ const mockData = {
     },
     "locationData": {
         "dinoLoc": {x:0,y:0,heading:0},
-        "carsLoc": [
-            {x:100,y:100,heading:0},
-            {x:-100,y:100,heading:1},
-            {x:-100,y:-100,heading:2}
-        ],
-        "peopleLoc": [
-            {x:80,y:80,heading:0},
-            {x:-80,y:80,heading:1},
-            {x:-80,y:-80,heading:2},
-            {x:80,y:-80,heading:3},
-        ]
+        "carLoc": {x:100,y:100,heading:0},
     },
     "perimeterData": {
-        "sections": [0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        "sections": [0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        "tripwireAlert": false
     },
     "clientData": {
         "clients": [
             {
                 "firstName": "Peggy",
                 "lastName": "Williams",
-                "id": "1578fe14-949d-4767-8202-b8050ba62a16"
+                "id": "1578fe14-949d-4767-8202-b8050ba62a16",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Morgan",
                 "lastName": "Gray",
-                "id": "ef5169f4-a9a3-4caf-a906-58a1b61a1c68"
+                "id": "ef5169f4-a9a3-4caf-a906-58a1b61a1c68",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Justin",
                 "lastName": "Mcgee",
-                "id": "402b1017-c922-4cd8-bc15-d2f5f1abd2d0"
+                "id": "402b1017-c922-4cd8-bc15-d2f5f1abd2d0",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Kristen",
                 "lastName": "Johnston",
-                "id": "4ba90c3b-cc60-4a20-9939-3da781e597bf"
+                "id": "4ba90c3b-cc60-4a20-9939-3da781e597bf",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Nicholas",
                 "lastName": "Clark",
-                "id": "0b1d0b8c-c485-4b0d-ac9c-80e3a728eb97"
+                "id": "0b1d0b8c-c485-4b0d-ac9c-80e3a728eb97",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Ronald",
                 "lastName": "Rodriguez",
-                "id": "9c725081-60f5-4708-881d-99ffd2b0b5d4"
+                "id": "9c725081-60f5-4708-881d-99ffd2b0b5d4",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "David",
                 "lastName": "Rivera",
-                "id": "72774ad8-8b9a-4dd2-9901-fc3c330e7555"
+                "id": "72774ad8-8b9a-4dd2-9901-fc3c330e7555",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Sherry",
                 "lastName": "Schultz",
-                "id": "3363b6cf-135d-4321-970f-9e66e31149b1"
+                "id": "3363b6cf-135d-4321-970f-9e66e31149b1",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Dale",
                 "lastName": "Garcia",
-                "id": "adefa301-0bb6-466d-8227-2aaa4babfbe9"
+                "id": "adefa301-0bb6-466d-8227-2aaa4babfbe9",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Donna",
                 "lastName": "Deleon",
-                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51"
+                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Donna",
                 "lastName": "Deleon",
-                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51"
+                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Donna",
                 "lastName": "Deleon",
-                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51"
+                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Donna",
                 "lastName": "Deleon",
-                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51"
+                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Donna",
                 "lastName": "Deleon",
-                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51"
+                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51",
+                "location": "kiosk"
                 },
                 {
                 "firstName": "Donna",
                 "lastName": "Deleon",
-                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51"
+                "id": "a4fdef50-9008-4d7e-b664-0bfe824cbe51",
+                "location": "kiosk"
                 }
         ]
     }
