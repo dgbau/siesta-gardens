3a57ee55d4356a2cc4b6e321691c0fe996e8ff80
farhangrouhi <frouhi@unm.edu>
2020-05-11 20:33:37 -0600

    Simulator added (Kinda)

--- server/model/Simulator.py
+++ server/model/Simulator.py
@@ -2,15 +2,15 @@ import time
 import numpy as np
 
 class Simulator:
-    _x_dino = _x[0]+buffer+np.random.random()*(_x[1]-x[0]-buffer*2)
-    _y_dino = _y[0]+buffer+np.random.random()*(_y[1]-y[0]-buffer*2)
-    _rad_dino = np.random.random()*(2*pi)
-
     _dino_buffer = 20
     _x_cage = [2150,3050]
     _y_cage = [2100,3900]
     _dino_step_size = 1
     _car_step_size = 1
+
+    _x_dino = _x_cage[0]+buffer+np.random.random()*(_x_cage[1]-_x_cage[0]-buffer*2)
+    _y_dino = _y_cage[0]+buffer+np.random.random()*(_y_cage[1]-_y_cage[0]-buffer*2)
+    _rad_dino = np.random.random()*(2*pi)
 
     _reached_dest = True
 
@@ -18,7 +18,7 @@ class Simulator:
         return _x_dino
     def get_y_dino():
         return _y_dino
-    
+
     _x_road = [3100,2600,2100,2100,3000]
     _y_road = [1550,1550,2050,3950,3950]
 
