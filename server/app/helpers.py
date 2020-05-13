fcf81ca7ee21c52cddc7d42674bc197115a3e3c6
David Johnson-Bau <dgbau@me.com>
2020-05-13 08:09:54 -0600

    added location to the users model in create_fake_users

--- server/app/helpers.py
+++ server/app/helpers.py
@@ -5,12 +5,16 @@ fake = Faker()
 def create_fake_users(count):
     users = []
     for i in range(count):
+        status = "queued" if random.random() < 0.5 else "touring"
+        location = ("exploring" if random.random() < 0.5 else "riding") if status == "touring" \
+            else ("ticketed" if random.random() < 0.5 else "arrived")
         users.append(
             {
                 "firstName": fake.first_name(),
                 "lastName": fake.last_name(),
                 "id": fake.uuid4(),
-                "status": "queued" if random.random() < 0.5 else "touring" 
+                "status": status,
+                "location": location
             }
         )
     return users
